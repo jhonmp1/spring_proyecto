40422bf44f9426ac50746ab6f1c18cbfe76ca3de
unknown <fabrizio.atiquipa@tecsup.edu.pe>
2021-05-27 15:43:26 -0500

    v1

---
+++ src/main/java/com/miempresa/Spring2TareaApplication.java
@@ -0,0 +1,13 @@
+package com.miempresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring2TareaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring2TareaApplication.class, args);
+	}
+
+}
